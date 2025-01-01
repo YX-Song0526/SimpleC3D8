30b9527c8ecdc7f1b78a3ab3484f2874f7bb631e
YX-Song0526 <muyu38324@163.com>
2025-01-01 23:42:10 +0800

    update

--- stiffness_matrix.py
+++ stiffness_matrix.py
@@ -4,6 +4,7 @@ from preprocess import num_dof, node_dof_idx
 # 材料属性
 E = 69e9  # 杨氏模量
 niu = 0.28  # 泊松比
+rho = 1
 
 # 弹性矩阵
 D = (E / (1 + niu) * (1 - 2 * niu)) * np.array([[1 - niu, niu, niu, 0, 0, 0],
@@ -97,3 +98,56 @@ def cal_K_total(node_coords, elements):
                 K_total[dof[i], dof[j]] += Ke[i, j]
 
     return K_total
+
+
+def cal_Me(node_coords):
+    """
+    计算单元质量质量矩阵
+
+    Args:
+        node_coords: 节点坐标矩阵 (num_nodes, 3)
+    """
+    Me = np.zeros((24, 24))  # 修改大小以适应 2D 单元
+    for gp_t, wt in zip(gauss_points, gauss_weights):
+        for gp_n, wn in zip(gauss_points, gauss_weights):
+            for gp_s, ws in zip(gauss_points, gauss_weights):
+
+                # 计算形函数
+                N = shape_func(gp_s, gp_n, gp_t)
+                N_matrix = np.zeros((3, 24))
+                N_matrix[0, 0::3] = N
+                N_matrix[1, 1::3] = N
+                N_matrix[2, 2::3] = N
+
+                # 雅可比矩阵计算
+                dN_dxi = shape_func_derivatives(gp_s, gp_n, gp_t)
+                J = dN_dxi.T @ node_coords
+                detJ = np.linalg.det(J)
+                if detJ <= 0:
+                    raise ValueError("Jacobian determinant is non-positive!")
+
+                # 质量矩阵累加
+                Me += rho * (N_matrix.T @ N_matrix) * detJ * ws * wn * wt
+    return Me
+
+
+def cal_M_total(node_coords, elements):
+    """
+    计算系统总体质量矩阵
+
+    Args:
+        elements: 单元连接矩阵 (num_elements, 4)
+        node_coords: 节点坐标矩阵 (num_nodes, 2)
+    """
+    M_total = np.zeros((num_dof, num_dof))
+    for element in elements:
+        ele_nodes = node_coords[element]
+        Me = cal_Me(ele_nodes)
+        # print(Me)
+        dof = node_dof_idx[element].reshape(24)
+        # print(dof)
+        for i in range(24):
+            for j in range(24):
+                M_total[dof[i], dof[j]] += Me[i, j]
+
+    return M_total
